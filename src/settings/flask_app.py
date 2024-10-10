698520e40e12e2aff10f0f30cd10451c46b42454
Jonas Santos <jonaskest@hotmail.com>
2024-10-10 14:58:17 -0300

    update

---
+++ src/settings/flask_app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from database.sessao import db
+from settings.config import Config
+from routes.transacao import register_routes
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()  # criar todas as tabelas
+
+    register_routes(app)
+
+    return app
